9470f768f86683aaa62306dfeaf0f85a61cc9cba
Lorena Sarkezi <lorena.sarkezi@syskit.com>
2022-01-17 22:13:44 +0100

    ProtectedRoute fix

--- ChatApp.ReactApp/src/components/protectedRoute/protectedRoute.tsx
+++ ChatApp.ReactApp/src/components/protectedRoute/protectedRoute.tsx
@@ -1,31 +1,36 @@
-import { useState } from 'react';
-import { useEffect } from 'react-dom/node_modules/@types/react';
-import { Redirect, Route, RouteProps } from 'react-router';
+import { useEffect, useState } from 'react';
+import { Redirect, Route } from 'react-router';
 import AuthHelpers from '../../misc/helpers';
 
 // https://stackoverflow.com/questions/47747754/how-to-rewrite-the-protected-private-route-using-typescript-and-react-router-4
 
-export type ProtectedRouteProps = {
-  isAuthenticated: boolean;
-  authenticationPath: string;
-} & RouteProps;
-
-export const ProtectedRoute: React.FC<RouteProps> = (props) => {
+const ProtectedRoute = props => {
 
   const [isAuthenticated , setIsAuthenticated] = useState<boolean | null>(null);
 
   const getAuthStatus = async () => {
-    return await AuthHelpers.checkIfAuthenticated();
+    const status = await AuthHelpers.checkIfAuthenticated();
+    setIsAuthenticated(status as boolean);
   }
 
-  // useEffect(() => {
-  //   setIsAuthenticated(getAuthStatus());
-  // })
+  useEffect(() => {
+    if(isAuthenticated === null){
+      getAuthStatus();
+    }
+    
+  }, [isAuthenticated])
 
-  if(isAuthenticated) {
-    return <Route {...props} />;
-  } 
-  else {
-    return <Redirect to={{ pathname: '/login' }} />;
+  if(isAuthenticated !== null){
+    if(isAuthenticated === true) {
+      return <Route {...props} />;
+    } 
+    else {
+      return <Redirect to={{ pathname: '/login' }} />;
+    }
+  }
+  else{
+    /// neki loading spinner
   }
 };
+
+export default ProtectedRoute;
